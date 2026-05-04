4abd6ad760a0191d1c5c8e1e5b496f001062e410
SNEHAL SAWANT <snellbhor@gmail.com>
2026-05-05 00:27:54 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,6 @@
+#include<stdio.h> //preprocessor
+int main()
+{
+    printf("Marvellous Infosystems\n");
+    return 0;
+}
---
+++ sizeof.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{   
+    char ch='A';
+    int i=11;
+    float f=90.99f;
+    double d = 78.67890;
+    printf("Size of all variables:\n");
+    printf("%d\n",sizeof(ch)); // 1 byte
+    printf("%d\n",sizeof(i));   // 4 byte
+    printf("%d\n",sizeof(f));   // 4 byte
+    printf("%d\n",sizeof(d));   //8 byte
+    return 0;
+}
